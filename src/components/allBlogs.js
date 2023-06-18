a36bc06dd03feeabaaaef1e1bfc802b7cfbbbfd5
Kareemm2 <132186526+KareemMohamed88@users.noreply.github.com>
2023-06-18 15:00:33 +0200

    test

--- src/components/allBlogs.js
+++ src/components/allBlogs.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
 const AllBlogs = () => {
-  const api = "http://localhost:3001";
+  const api = "https://topwwheal-server.onrender.com";
   const [blog, setBlog] = useState([]);
   const [pageCount, setPageCount] = useState(1);
   const handleNext = () => {
