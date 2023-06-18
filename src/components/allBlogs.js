f00a080e7eb32c8ba468c97f0047d3b3218dbcd1
Kareemm2 <132186526+KareemMohamed88@users.noreply.github.com>
2023-06-18 15:03:41 +0200

    Update allBlogs.js

--- src/components/allBlogs.js
+++ src/components/allBlogs.js
@@ -37,7 +37,7 @@ const AllBlogs = () => {
 
               <a
                 href={`/blogs/${blog._id}`}
-                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-400 rounded-lg hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-400 rounded-lg hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 "
               >
                 Read more
                 <svg
