0398cef015b9373c414b9650130565d62fedba44
Kareemm2 <132186526+KareemMohamed88@users.noreply.github.com>
2023-06-20 01:35:51 +0200

    dfis

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Nav from '../components/nav'
+import Header from '../components/header'
 import AllBlogs from '../components/allBlogs'
 const Home = () => {
   
   return (
     <React.Fragment>
       <div className="container-fluid">
+        <Header/>
         <Nav/>
         <AllBlogs/>
       </div>
