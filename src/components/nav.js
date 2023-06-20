4b8c82e8e5517b9895d562126973887aa6d59d90
Kareemm2 <132186526+KareemMohamed88@users.noreply.github.com>
2023-06-20 15:08:19 +0200

    seo

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,18 +1,15 @@
-import "./nav.css";
 const Navbar = () => {
 
-
   return (
-    <div >
-
-  <div className="nav">
-    <ul>
-      <li><a href="#w">Home</a></li>
-      <li><a href="#w">About us</a></li>
-      <li><a href="#w">Contact us</a></li>
-      <li><a href="#w">Follow us</a></li>
-    </ul>
-  </div>
+    <div style={{width: "100%", height: "60px", display: "flex", justifyContent: 'space-between', alignItems: "center", padding: "0 18%"}}>
+      <div className="logo">
+        <h1 style={{fontSize: "20px"}}>Top Wheal</h1>
+      </div>
+      <ul>
+        <li style={{display: "inline-block", margin: "0 10px"}}><a href="#">Home</a></li>
+        <li style={{display: "inline-block", margin: "0 10px"}}><a href="#">About us</a></li>
+        <li style={{display: "inline-block", margin: "0 10px"}}><a href="#"></a>Contact us</li>
+      </ul>
     </div>
   );
 };
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,14 @@
 import React from "react";
-import Nav from '../components/nav'
-import Header from '../components/header'
-import AllBlogs from '../components/allBlogs'
+import Nav from "../components/nav";
+import Header from "../components/header";
+import AllBlogs from "../components/allBlogs";
 const Home = () => {
-  
   return (
     <React.Fragment>
       <div className="container-fluid">
-        <Header/>
-        <Nav/>
-        <AllBlogs/>
+        <Nav />
+        <Header />
+        <AllBlogs />
       </div>
     </React.Fragment>
   );
