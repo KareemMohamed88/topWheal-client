792c4049d4ee795daaa60c8650b33264f7ef50f1
Kareemm2 <132186526+KareemMohamed88@users.noreply.github.com>
2023-06-21 16:58:58 +0200

    SEO

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,7 +3,7 @@ const Navbar = () => {
   return (
     <div style={{width: "100%", height: "60px", display: "flex", justifyContent: 'space-between', alignItems: "center", padding: "0 18%"}}>
       <div className="logo">
-        <h1 style={{fontSize: "20px"}}>Top Wheal</h1>
+        <h1 style={{fontSize: "20px"}}>supercharger</h1>
       </div>
       <ul>
         <li style={{display: "inline-block", margin: "0 10px"}}><a href="#">Home</a></li>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Nav from "../components/nav";
-import Header from "../components/header";
 import AllBlogs from "../components/allBlogs";
 const Home = () => {
   return (
     <React.Fragment>
       <div className="container-fluid">
         <Nav />
-        <Header />
+        <div className="h-96 flex justify-center items-center text-center bg-indigo-300">
+          <h2 className="text-3xl mb-5 text-white font-bold">supercharger</h2>
+      </div>
         <AllBlogs />
       </div>
     </React.Fragment>
